7a33ec4cfc63ed4eedc3469879f3aa187a8f6301
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2021-01-03 23:47:40 -0500

    display letter by typing on the keyboard. Clicking buttons does nothing now.

--- js/main.js
+++ js/main.js
@@ -1,43 +1,20 @@
 const area = document.querySelector("textarea");
-const keys = document.querySelectorAll(".key");
-const enter = document.querySelector(".enter");
-const deleteKey = document.querySelector(".delete");
-const clear = document.querySelector(".clear");
-const space = document.querySelector(".space");
 
-// Keep the cursor in the textarea when it's out of focus
-function keepCursor() {
-  area.focus();
-  area.setSelectionRange(area.value.length, area.value.length);
-}
-
-// Click to input key
-keys.forEach((key) => {
-  key.addEventListener("click", () => {
-    area.value += key.textContent;
-    keepCursor();
-  });
-});
-
-// Add a new line
-enter.addEventListener("click", () => {
-  area.value += "\n";
-  keepCursor();
-});
-
-// Delete a single character on click
-deleteKey.addEventListener("click", () => {
-  area.value = area.value.slice(0, -1);
-  keepCursor();
-});
-
-// Delete all characters on click
-clear.addEventListener("click", () => {
-  area.value = "";
-  keepCursor();
-});
-// Add space between words
-space.addEventListener("click", () => {
-  area.value += " ";
-  keepCursor();
+area.addEventListener("keydown", (event) => {
+  if (event.key === "q" || event.key === "Q") {
+    setTimeout(() => {
+      event.target.value += "ࡒ";
+    }, 0);
+    event.preventDefault();
+  } else if (event.key === "w" || event.key === "W") {
+    setTimeout(() => {
+      event.target.value += "ࡅ";
+    }, 0);
+    event.preventDefault();
+  } else if (event.key === "e" || event.key === "E") {
+    setTimeout(() => {
+      event.target.value += "ࡏ";
+    }, 0);
+    event.preventDefault();
+  }
 });
