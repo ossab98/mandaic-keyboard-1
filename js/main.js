c59c5bb34bfa1cdeac6fa0fd961955689ebf8a61
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2021-01-04 19:27:25 -0500

    Finished replacing Latin Alphabet with Mandaic.

--- js/main.js
+++ js/main.js
@@ -6,14 +6,200 @@ area.addEventListener("keydown", (event) => {
       event.target.value += "ࡒ";
     }, 0);
     event.preventDefault();
-  } else if (event.key === "w" || event.key === "W") {
+  }
+
+  if (event.key === "w" || event.key === "W") {
     setTimeout(() => {
       event.target.value += "ࡅ";
     }, 0);
     event.preventDefault();
-  } else if (event.key === "e" || event.key === "E") {
+  }
+
+  if (event.key === "e" || event.key === "E") {
     setTimeout(() => {
       event.target.value += "ࡏ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "r" || event.key === "R") {
+    setTimeout(() => {
+      event.target.value += "ࡓ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "t" || event.key === "T") {
+    setTimeout(() => {
+      event.target.value += "ࡕ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "y" || event.key === "Y") {
+    setTimeout(() => {
+      event.target.value += "ࡈ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "u" || event.key === "U") {
+    setTimeout(() => {
+      event.target.value += "ࡗ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "i" || event.key === "I") {
+    setTimeout(() => {
+      event.target.value += "ࡉ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "o" || event.key === "O") {
+    setTimeout(() => {
+      event.target.value += "ࡇ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "p" || event.key === "P") {
+    setTimeout(() => {
+      event.target.value += "࡚";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "a" || event.key === "A") {
+    setTimeout(() => {
+      event.target.value += "ࡀ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "s" || event.key === "S") {
+    setTimeout(() => {
+      event.target.value += "ࡎ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "d" || event.key === "D") {
+    setTimeout(() => {
+      event.target.value += "ࡃ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "f" || event.key === "F") {
+    setTimeout(() => {
+      event.target.value += "ࡐ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "g" || event.key === "G") {
+    setTimeout(() => {
+      event.target.value += "ࡂ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "h" || event.key === "H") {
+    setTimeout(() => {
+      event.target.value += "ࡄ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "j" || event.key === "J") {
+    setTimeout(() => {
+      event.target.value += "ࡖ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "k" || event.key === "K") {
+    setTimeout(() => {
+      event.target.value += "ࡊ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "l" || event.key === "L") {
+    setTimeout(() => {
+      event.target.value += "ࡋ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === ";") {
+    setTimeout(() => {
+      event.target.value += "࡙";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "z" || event.key === "Z") {
+    setTimeout(() => {
+      event.target.value += "ࡆ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "x" || event.key === "X") {
+    setTimeout(() => {
+      event.target.value += "ࡔ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "c" || event.key === "C") {
+    setTimeout(() => {
+      event.target.value += "ࡑ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "v" || event.key === "V") {
+    setTimeout(() => {
+      event.target.value += "ࡘ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "b" || event.key === "B") {
+    setTimeout(() => {
+      event.target.value += "ࡁ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "n" || event.key === "N") {
+    setTimeout(() => {
+      event.target.value += "ࡍ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === "m" || event.key === "M") {
+    setTimeout(() => {
+      event.target.value += "ࡌ";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === ",") {
+    setTimeout(() => {
+      event.target.value += "࡛";
+    }, 0);
+    event.preventDefault();
+  }
+
+  if (event.key === ".") {
+    setTimeout(() => {
+      event.target.value += "࡞";
     }, 0);
     event.preventDefault();
   }
